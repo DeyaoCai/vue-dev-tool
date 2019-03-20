ad45c7f538c1ac24113995e1faaa13cc4b8e3f91
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-20 20:18:24 +0800

    no message

---
+++ ctools.conf/mainJsTemp.js
@@ -0,0 +1,6 @@
+//内部项目样式
+import '@common/assets/iconfont/iconfont.css'
+import '@common/assets/iconfont/iconfont.js'
+
+//项目集成
+import {init} from 'wxm-app'
--- src/main.js
+++ src/main.js
@@ -16,4 +16,3 @@ export default new Vue({
   components: { App },
   template: '<App/>'
 });
-
