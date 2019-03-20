7e9c96da18d2ba500fa6cedc92b692dcc7a40595
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-21 01:51:50 +0800

    no message

--- ctools.conf/bizConfs/vucConf.js
+++ ctools.conf/bizConfs/vucConf.js
@@ -2,10 +2,9 @@ const fs = require("fs");
 const cwd = process.cwd();
 
 function getMainJsTemplateConf() {
-  const baseContent = fs.readFileSync(`${cwd}/ctools.conf/mainJsTemplate/mainJsTemp.vuc.js`);
   return {
     outPutPath: `/src/main.js`,
-    content: baseContent
+    content: fs.readFileSync(`${cwd}/ctools.conf/mainJsTemplate/mainJsTemp.vuc.js`)
   }
 }
 
@@ -17,6 +16,10 @@ const alias = {
 };
 const defaultBranch = `master`;
 const repertoryList = [
+  {
+    repertory: "https://github.com/DeyaoCai/electron-pc-desktop.git",
+    branch: defaultBranch,
+  },
   {
     repertory: "https://github.com/DeyaoCai/vuc.git",
     branch: defaultBranch,
@@ -48,24 +51,22 @@ const packageJson = {
     "not ie <= 8"
   ],
   scripts: {
+    ht: "node ./sections/electron-pc-desktop/proxy/ht.js",
+    desktopServe: "node ./sections/electron-pc-desktop/app.js",
+    desktop: "electron ./sections/electron-pc-desktop/main.js",
     // 拉取代码
-    getCodes: "node ./sections/ctools/bin/mergePackgeJson.js getCodes --branch-ISSUES-5765",
+    getCodes: "node ./sections/ctools/bin/mergePackgeJson.js getCodes",
     // 开启服务
     devCdy: "webpack-dev-server --inline --progress --config build/webpack.dev.conf.cdy.js",
     // 单独的更新 package.json
-    updatePackageJson: "node ./tem-biz/ctools/bin/mergePackgeJson.js updatePackageJson",
-
+    updatePackageJson: "node ./sections/ctools/bin/mergePackgeJson.js updatePackageJson",
     testCtools: "node ./sections/ctools/bin/ctools.js watch",
     testCtoolsRead: "node ./sections/ctools/bin/ctools.js read--vucUi-cdyUtilsAndVucUi",
     watch: "node ./script/watch.js",
     push: "push --todev",
     read: "ctools read",
-    dev: "webpack-dev-server --inline --progress --config build/webpack.dev.conf.js",
     ndev: "webpack-dev-server --inline --progress --config build/webpack.ndev.conf.js",
     unit: "cross-env BABEL_ENV=test karma start test/unit/karma.conf.js --single-run",
-    test: "npm run unit",
-    lint: "eslint --ext .js,.vue src test/unit",
-    build: "node build/build.js"
   }
 };
 module.exports = {
