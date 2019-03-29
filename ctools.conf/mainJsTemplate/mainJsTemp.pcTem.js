bfe8995ec9d84f412bf0bd965b61163941428c1d
DeyaoCai <360867514@qq.com>
2019-03-29 18:26:35 +0800

    no message

--- ctools.conf/mainJsTemplate/mainJsTemp.pcTem.js
+++ ctools.conf/mainJsTemplate/mainJsTemp.pcTem.js
@@ -2,6 +2,7 @@
 import '@common/assets/iconfont/iconfont.css'
 import '@common/assets/iconfont/iconfont.js'
 import {init} from 'wxm-pc'
+init([config], {autoLogin: true, useLang: true});
 <-ctools-replace-tag-0>
 <-ctools-replace-tag-1>
 <-ctools-replace-tag-2>
